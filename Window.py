663c4814818543f2d793101649e6fd1280eb6648
Matteo Chabbert <chabb.mat@gmail.com>
2023-11-20 15:06:56 +0100

    update

---
+++ Window.py
@@ -0,0 +1,95 @@
+import pygame
+import sys
+from abc import ABC, abstractmethod
+from Node import Node
+
+# Définition des couleurs
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+class Window(ABC):
+    def __init__(self, largeur, hauteur, titre):
+        pygame.init()
+        self.largeur = largeur
+        self.hauteur = hauteur
+        self.titre = titre
+        self.font = pygame.font.Font(None, 36)
+
+    @abstractmethod
+    def afficher_contenu(self):
+        pass
+
+    def afficher_texte(self, texte, x, y):
+        texte_surface = self.font.render(texte, True, WHITE)
+        texte_rect = texte_surface.get_rect(center=(x, y))
+        self.screen.blit(texte_surface, texte_rect)
+
+
+class Menu(Window):
+    def __init__(self, largeur, hauteur, titre):
+        super().__init__(largeur, hauteur, titre)
+
+    def afficher_contenu(self):
+        self.afficher_texte("Menu Principal", self.largeur // 2, self.hauteur // 4)
+        self.afficher_texte("Jouer", self.largeur // 2, self.hauteur // 2)
+        self.afficher_texte("Options", self.largeur // 2, self.hauteur // 2 + 50)
+        self.afficher_texte("Quitter", self.largeur // 2, self.hauteur // 2 + 100)
+
+    def gerer_clic(self, pos_souris):
+        if self.bouton_clic(self.largeur // 2 - 50, self.hauteur // 2 - 25, 100, 50, pos_souris):
+            print("Bouton Jouer cliqué")
+        elif self.bouton_clic(self.largeur // 2 - 50, self.hauteur // 2 + 25, 100, 50, pos_souris):
+            print("Bouton Options cliqué")
+        elif self.bouton_clic(self.largeur // 2 - 50, self.hauteur // 2 + 75, 100, 50, pos_souris):
+            pygame.quit()
+            sys.exit()
+
+
+class MAP(Window):
+    def __init__(self, largeur, hauteur, titre):
+        super().__init__(largeur, hauteur, titre)
+        nb_lignes = 10
+        nb_colonnes = 10
+        # Calcul de la taille des spots en fonction de la fenêtre
+        self.taille_spot = min(largeur // nb_colonnes, hauteur // nb_lignes)
+        #self.matrice_nodes = [[None for _ in range(nb_colonnes)] for _ in range(nb_lignes)]
+        self.node = []
+        self.connections = []
+        self.screen = pygame.display.set_mode((largeur, hauteur))
+        self.text_niveau = ""
+        pygame.display.set_caption(titre)
+    
+    # Fonction pour dessiner les nœuds
+    def draw_nodes(self, node):
+        pygame.draw.circle(self.screen, node.couleur, (node.x, node.y), 10)
+    
+    # Fonction pour dessiner les connexions
+    def draw_connections(self, connection):
+        start_pos = (connection[0].x, connection[0].y)
+        end_pos = (connection[1].x, connection[1].y)
+        pygame.draw.line(self.screen, WHITE, start_pos, end_pos, 2)
+
+    def initialiser_niveau(self, n):
+        # Initialisation des nœuds
+        if n == 1:
+            self.nodes = [
+                Node(100, 100, RED),
+                Node(200, 200, GREEN),
+                Node(300, 300, BLUE),
+                Node(400, 400, RED),
+                Node(500, 500, GREEN),
+                Node(600, 100, BLUE),
+            ]
+        self.text_niveau = f"Niveau {n}"
+
+    def afficher_contenu(self):
+        self.screen.fill(BLACK)
+        self.afficher_texte("Drag and Drop Nodes", self.largeur // 2, self.hauteur // 40)
+        self.afficher_texte(self.text_niveau, self.largeur // 2, self.hauteur // 15)
+        for node in self.nodes:
+            self.draw_nodes(node)
+        for connection in self.connections:
+            self.draw_connections(connection)
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import pygame
 import sys
-
-# Initialisation de Pygame
-pygame.init()
+from Window import MAP
+from Node import Node
 
 # Définition des couleurs
 WHITE = (255, 255, 255)
@@ -11,108 +10,64 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-# Définition de la taille de la fenêtre
-WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Drag and Drop Nodes")
+# Exemple d'utilisation
+if __name__ == "__main__":
+    # Initialisation de Pygame
+    pygame.init()
+    map = MAP(800, 600, "Jeu")
+    map.initialiser_niveau(1)
+    # Boucle principale
+    dragging = False
+    current_connection = []
 
-# Définition de la classe Node
-class Node:
-    def __init__(self, x, y, couleur):
-        self.x = x
-        self.y = y
-        self.connections = {couleur:1}
-        self.last_connection = None # Pour changement de couleur >= 
-        self.couleur = couleur
-    
-    # change si >
-    def change_color(self):
-        print(max(self.connections, key=self.connections.get))
-        self.couleur = max(self.connections, key = self.connections.get)
-    
-    """
-    # change si >=
-    def change_color(self):
-        print(max(self.connections, key=self.connections.get))
-        self.couleur = self.last_connection if list(self.connections.values()).count(max(self.connections.values()))>1 else max(self.connections, key = self.connections.get)
-"""
+    while True:
 
-# Initialisation des nœuds
-nodes = [
-    Node(100, 100, RED),
-    Node(200, 200, GREEN),
-    Node(300, 300, BLUE),
-    Node(400, 400, RED),
-    Node(500, 500, GREEN),
-    Node(600, 100, BLUE),
-]
-
-# Liste pour stocker les liaisons entre les nœuds
-connections = []
-
-# Fonction pour dessiner les nœuds
-def draw_nodes():
-    for node in nodes:
-        pygame.draw.circle(screen, node.couleur, (node.x, node.y), 10)
-
-# Fonction pour dessiner les connexions
-def draw_connections():
-    for connection in connections:
-        start_pos = (connection[0].x, connection[0].y)
-        end_pos = (connection[1].x, connection[1].y)
-        pygame.draw.line(screen, WHITE, start_pos, end_pos, 2)
-        
-# Boucle principale
-running = True
-dragging = False
-current_connection = []
-
-while running:
-    screen.fill(BLACK)
-
-    # Gestion des événements
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:  # Clic gauche
-                for node in nodes:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # Clic gauche
+                    for node in map.nodes:
+                        if pygame.Rect(node.x - 10, node.y - 10, 20, 20).collidepoint(event.pos):
+                            dragging = True
+                            current_connection = [node, event.pos]
+                            break
+            elif event.type == pygame.MOUSEMOTION and dragging:
+                current_connection[1] = event.pos
+            elif event.type == pygame.MOUSEBUTTONUP and dragging:
+                dragging = False
+                # Vérifie si le drop est sur un nœud, sinon supprime la connexion
+                drop_on_node = False
+                for node in map.nodes:   
                     if pygame.Rect(node.x - 10, node.y - 10, 20, 20).collidepoint(event.pos):
-                        dragging = True
-                        current_connection = [node, event.pos]
+                        # Autorise que une connection entre 2 noeuds
+                        if [current_connection[0], node] in map.connections :
+                            current_connection = []
+                            print("Lien déjà existent")
+                            break
+                        map.connections.append([current_connection[0], node])
+                        drop_on_node = True
+                        # stock la couleur du noeud lie
+                        if current_connection[0].couleur in node.connections:
+                            node.connections[(current_connection[0].couleur)] += 1
+                        else:
+                            node.connections[(current_connection[0].couleur)] = 1
+                        # change la couleur du noeud en fonction de ses liens (définir intéraction pour nombre de liens égaux)
+                        node.last_connection = current_connection[0].couleur
+                        node.change_color()
+                        print(map.connections)
                         break
-        elif event.type == pygame.MOUSEMOTION and dragging:
-            current_connection[1] = event.pos
-        elif event.type == pygame.MOUSEBUTTONUP and dragging:
-            dragging = False
-            # Vérifie si le drop est sur un nœud, sinon supprime la connexion
-            drop_on_node = False
-            for node in nodes:
-                if pygame.Rect(node.x - 10, node.y - 10, 20, 20).collidepoint(event.pos):
-                    connections.append([current_connection[0], node])
-                    drop_on_node = True
-                    # stock la couleur du noeud lie
-                    if current_connection[0].couleur in node.connections:
-                        node.connections[(current_connection[0].couleur)] += 1
-                    else:
-                        node.connections[(current_connection[0].couleur)] = 1
-                    # change la couleur du noeud en fonction de ses liens (définir intéraction pour nombre de liens égaux)
-                    node.last_connection = current_connection[0].couleur
-                    node.change_color()
-                    break
-            if not drop_on_node:
-                current_connection = []
+                if not drop_on_node:
+                    current_connection = []
+
+        map.afficher_contenu()
             
+        # Dessine la ligne en cours de drag
+        if dragging:
+            print(map.screen,"/", WHITE, (current_connection[0].x, current_connection[0].y),"/", current_connection[1])
+            pygame.draw.line(map.screen, WHITE, (current_connection[0].x, current_connection[0].y), current_connection[1], 2)
+    
 
-    draw_nodes()
-    draw_connections()
-
-    # Dessine la ligne en cours de drag
-    if dragging:
-        print(screen,"/", WHITE, (current_connection[0].x, current_connection[0].y),"/", current_connection[1])
-        pygame.draw.line(screen, WHITE, (current_connection[0].x, current_connection[0].y), current_connection[1], 2)
-    pygame.display.flip()
-
-# Quitte Pygame
-pygame.quit()
-sys.exit()
+        pygame.display.flip()
+    
