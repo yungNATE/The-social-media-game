018d63941888bcb934257940de695942a9eac7ef
yungNATE <nathan.martinigol@gmail.com>
2023-11-22 17:53:34 +0100

    correction de bug

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ if __name__ == "__main__":
     current_node = None
     cursor_position = None
     cursor_over_max_limit = False
+    limited_cursor_position = (0,0)
 
     while True:
 
@@ -33,8 +34,10 @@ if __name__ == "__main__":
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  # Clic gauche
+
                     for node in map.nodes:
-                        if pygame.Rect(node.x - node.size, node.y - node.size, 20, 20).collidepoint(event.pos):
+                        reachedNode = pygame.draw.circle(map.screen, (0,0,0), (node.x, node.y), node.size).collidepoint(event.pos)
+                        if reachedNode:
                             dragging = True
                             current_node = node
                             cursor_position = event.pos
@@ -46,19 +49,18 @@ if __name__ == "__main__":
             elif event.type == pygame.MOUSEBUTTONUP and dragging:
                 dragging = False
                 drop_on_node = False
-                if(cursor_over_max_limit): 
-                    cursor_over_max_limit = False
-                    break
+                if(cursor_over_max_limit): cursor_pos = limited_cursor_position
+                else:                      cursor_pos = cursor_position
 
                 # Vérifie si le drop est sur un nœud, sinon supprime la connexion
                 for node in map.nodes:
-                    reached_node = pygame.Rect(node.x - 10, node.y - 10, 20, 20).collidepoint(event.pos)
-                    if reached_node:
+                    reachedNode = pygame.draw.circle(map.screen, node.couleur, (node.x, node.y), node.size).collidepoint(cursor_pos)
+                    if reachedNode:
                         drop_on_node = True
                         
                         isFollowSuccessful = current_node.follow(node)
-                        if not isFollowSuccessful: break
-                        else: map.update_nodes()
+                        if isFollowSuccessful: map.update_nodes()
+                        else: break
                 
                 if not drop_on_node:
                     current_node = None
@@ -74,19 +76,17 @@ if __name__ == "__main__":
             grand_oppose_length = cursor_position[1] - current_node.y
             grand_hypotenuse_length = math.sqrt(grand_adjacent_length**2 + grand_oppose_length**2)
             
-            if(grand_hypotenuse_length == 0): grand_hypotenuse_length = 0.0001
-
+            if(grand_hypotenuse_length == 0): grand_hypotenuse_length = 0.0001 # évite la division par 0
             angle = math.asin(grand_oppose_length / grand_hypotenuse_length)
 
             petit_hypothenus_length = node.reach
 
             modified_cos = math.cos(angle) if cursor_position[0] > current_node.x else -math.cos(angle) 
-            #pacth d'un bug, je suis trop mauvais en math pour comprendre
+            # patch d'un bug, je suis trop mauvais en math pour comprendre
 
             petit_adjacent_length = petit_hypothenus_length * modified_cos
             petit_oppose_length = petit_hypothenus_length * math.sin(angle)
 
-            print(math.sin(angle))
 
             if(grand_hypotenuse_length > petit_hypothenus_length):
                 virtual_point_to_draw_line_with_max_length = (current_node.x + petit_adjacent_length, current_node.y + petit_oppose_length)
@@ -94,6 +94,8 @@ if __name__ == "__main__":
             else:
                 virtual_point_to_draw_line_with_max_length = cursor_position
                 cursor_over_max_limit = False
+
+            limited_cursor_position = virtual_point_to_draw_line_with_max_length
 
             current_node_position = (current_node.x, current_node.y)
             # pygame.draw.line(map.screen, WHITE, current_node_position, virtual_point_to_draw_line_with_max_length, 2)
